9b55222c25c639b0dbbab8256df5de1074e19b47
manuel100472191 <100472191@alumnos.uc3m.es>
2023-09-11 12:04:19 +0200

    Creando la clase base de datos para acceder y modificar los datos

--- base_de_datos.py
+++ base_de_datos.py
@@ -4,3 +4,26 @@ import sqlite3
 class Db:
     def __init__(self):
         self.db_name = "base_de_datos.db"
+        self.connection = sqlite3.connect(self.db_name)
+        self.cursor = self.connection.cursor()
+
+    def create_db(self):
+        self.cursor.execute("DROP TABLE users;")
+        self.cursor.execute(""
+                            "CREATE TABLE users ("
+                            "user_id VARCHAR2(100) PRIMARY KEY,"
+                            "password VARCHAR2(255) NOT NULL"
+                            ");")
+
+    def add_user(self, user_name, password):
+        self.cursor.execute("INSERT INTO users VALUES (?, ?);", (user_name, password))
+
+    def populate_db(self):
+        self.cursor.execute("INSERT INTO users VALUES ('delamola19', 'contraseña');")
+        self.cursor.execute("INSERT INTO users VALUES ('user1', 'user1_pass');")
+
+    def view_data(self):
+        rows = self.cursor.execute("SELECT * from users").fetchall()
+        print(rows)
+        for row in rows:
+            print(f"user: {row[0]} ---- password: {row[1]}")
--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 import sqlite3
+from base_de_datos import Db
 
-connection = sqlite3.connect("base_de_datos.db")
-print(connection.total_changes)
+base_de_datos = Db()
 
-cursor = connection.cursor()
-cursor.execute("DROP TABLE users;")
-cursor.execute(""
-               "CREATE TABLE users ("
-               "user_id VARCHAR2(100) PRIMARY KEY,"
-               "password VARCHAR2(255) NOT NULL"
-               ");")
+base_de_datos.create_db()
+base_de_datos.populate_db()
+base_de_datos.view_data()
 
-cursor.execute("INSERT INTO users VALUES ('delamola19', 'contraseña');")
-cursor.execute("INSERT INTO users VALUES ('user1', 'user1_pass');")
-
-user = "delamola19"
-rows = cursor.execute(f"SELECT * FROM users WHERE user_id = ?", (user,)).fetchall()
-print(rows)
+# connection = sqlite3.connect("base_de_datos.db")
+# print(connection.total_changes)
+#
+# cursor = connection.cursor()
+# cursor.execute("DROP TABLE users;")
+# cursor.execute(""
+#                "CREATE TABLE users ("
+#                "user_id VARCHAR2(100) PRIMARY KEY,"
+#                "password VARCHAR2(255) NOT NULL"
+#                ");")
+#
+# cursor.execute("INSERT INTO users VALUES ('delamola19', 'contraseña');")
+# cursor.execute("INSERT INTO users VALUES ('user1', 'user1_pass');")
+#
+# user = "delamola19"
+# rows = cursor.execute("SELECT * from users").fetchall()
+# # rows = cursor.execute(f"SELECT * FROM users WHERE user_id = ?", (user,)).fetchall()
+# print(rows)
