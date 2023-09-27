b8f10fa9a318ec0361ddaf44451f08ce7842fc99
manuel100472191 <100472191@alumnos.uc3m.es>
2023-09-27 16:35:46 +0200

    Comentar algunas funciones

--- base_de_datos.py
+++ base_de_datos.py
@@ -4,18 +4,21 @@ from cryptobro import Crypto_bro
 
 class Db:
     def __init__(self):
+        """ Creates the connection with the database and with the crypto functions"""
         self.db_name = "base_de_datos.db"
         self.connection = sqlite3.connect(self.db_name)
         self.cursor = self.connection.cursor()
         self.crypto = Crypto_bro()
 
     def reset_db(self):
+        """ Resets the database to a initial state with some users and messages"""
         self.delete_db()
         self.create_db()
         self.populate_users()
         self.populate_messages()
 
     def create_db(self):
+        """ Creates the tables of the database """
         self.cursor.execute(""
                             "CREATE TABLE users ("
                             "telephone_number CHAR(9) PRIMARY KEY,"
@@ -38,21 +41,24 @@ class Db:
                             ");")
 
     def delete_db(self):
+        """ Deletes the tables from teh database """
         self.cursor.execute("DROP TABLE users;")
         self.cursor.execute("DROP TABLE messages;")
 
     def add_user(self, telephone, password, name, surname, email):
+        """ Adds a user to the database given the correct data"""
+        # Generates the salt and the password token for the user
         salt, password_token = self.crypto.create_password(password)
+        # Stores the data into the database
         self.cursor.execute("INSERT INTO users VALUES (?, ?, ?, ?, ?, ?);",
                             (telephone, password_token, name, surname, email, salt))
         self.connection.commit()
 
     def validate_user(self, telephone, password):
+        # Validates the log-in information of a user
         rows = list(self.cursor.execute("SELECT telephone_number, password_token, salt "
                                         "FROM users WHERE telephone_number = ?;"
                                         , (telephone,)))
-        if len(rows) != 1:
-            return False
         if self.crypto.verify_password(password, rows[0][2], rows[0][1]):
             return True
         return False
--- cryptobro.py
+++ cryptobro.py
@@ -8,7 +8,8 @@ import base64
 class Crypto_bro:
     @staticmethod
     def create_password(password: str) -> (str, str):
-        salt = os.urandom(16)  # Crea salt de 16 bytes
+        """ Generates the password_token that is going to be stored in the database """
+        salt = os.urandom(16)  # Generates a random salt for the user
         kdf = Scrypt(
             salt=salt,
             length=256,
@@ -16,11 +17,14 @@ class Crypto_bro:
             r=8,
             p=1
         )
+        # Returns the salt and the password in base64
         return base64.encodebytes(salt).decode('utf8'), \
             base64.encodebytes(kdf.derive(bytes(password, encoding='utf8'))).decode('utf8')
 
     @staticmethod
     def verify_password(password: str, salt: str, key: str) -> bool:
+        """ Verifies given a salt, a key, and a password that the password is correct"""
+        # Converts the salt and the key from base 64 into bytes and the password from utf8 into bytes
         password = bytes(password, encoding='utf8')
         salt = base64.decodebytes(bytes(salt, encoding='utf8'))
         key = base64.decodebytes(bytes(key, encoding='utf8'))
@@ -31,6 +35,7 @@ class Crypto_bro:
             r=8,
             p=1
         )
+        # Checks that the keys match to see if the password is valid
         try:
             kdf.verify(password, key)
         except cryptography.exceptions.InvalidKey:
